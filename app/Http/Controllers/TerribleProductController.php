80e3da15ed46e866e4dde449fe9c834ee5b99614
Shengyou Fan <shengyoufan@gmail.com>
2018-11-15 00:34:17 +0800

    增加 TerribleProductController 當範例

---
+++ app/Http/Controllers/TerribleProductController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Http\Requests\ProductRequest;
+use App\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class TerribleProductController extends Controller{
+
+    private function doSomething($request){
+        $attributes=[]
+        ;
+        foreach ($request->all() as $key => $value) {
+            $attributes[$key] = $value;
+        }return$attributes;}
+
+    public function index(){
+        $products = $this->product->orderBy('created_at', 'DESC')->get();
+        $data=['products'=>$products];
+        return view('products.index', $data);
+    }
+
+
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+    protected $product;
+
+    public function create(Category $category){
+        $categories =$category->all();
+$data = ['categories'=> $categories];
+        return view('products.create', $data);
+
+
+
+    }
+
+
+
+    public function store(ProductRequest $request) {
+        $attributes = $this->doSomething($request);$this->product->create($attributes);
+        return redirect()->route('products.index');}
+
+
+
+}
